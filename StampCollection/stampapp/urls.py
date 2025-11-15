acc6e8168dc7045e7d903dc5f328b773a6ce21aa
N-Ruma <23026170@muroran-it.ac.jp>
2025-11-15 05:54:23 +0000

    Initial Commit

--- StampCollection/stampapp/urls.py
+++ StampCollection/stampapp/urls.py
@@ -4,6 +4,4 @@ from . import views
 
 urlpatterns = [
     path("", views.home_view, name="home"),
-    path("google_map/", views.google_map_view, name="google_map"),
-    path("leafret_map/", views.leafret_map_view, name="leafret_map"),
 ]
--- StampCollection/stampapp/views.py
+++ StampCollection/stampapp/views.py
@@ -5,15 +5,3 @@ def home_view(request):
     template_name = "stampapp/home_view.html"
     context = {}
     return render(request, template_name, context)
-
-@login_required
-def google_map_view(request):
-    template_name = "stampapp/google_map_view.html"
-    context = {}
-    return render(request, template_name, context)
-
-@login_required
-def leafret_map_view(request):
-    template_name = "stampapp/leafret_map_view.html"
-    context = {}
-    return render(request, template_name, context)
