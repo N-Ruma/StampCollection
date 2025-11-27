c22d29e08cc99cdb34d9bbc8b3f56818e2ef7c51
N-Ruma <23026170@muroran-it.ac.jp>
2025-11-27 10:06:26 +0000

    StampPinモデルのフィールドにスタンプ画像フィールドを追加しました. - スタンプ画像はデータベースに保存する際，自動でUUID化して保存します.

--- StampCollection/StampCollection/settings.py
+++ StampCollection/StampCollection/settings.py
@@ -118,6 +118,6 @@ MEDIA_URL = "/media/"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL = "login"
-LOGIN_REDIRECT_URL = ""
-LOGOUT_REDIRECT_URL = "login"
+LOGIN_URL = "/login"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/login"
--- StampCollection/stampapp/models.py
+++ StampCollection/stampapp/models.py
@@ -1,31 +1,53 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from uuid import uuid4
 
 User = get_user_model()
 
-class StampPin(models.Model):
+class StampPin(models.Model):    
+    def get_uuid_image_path(self, filename: str) -> str:
+        '''画像パスをUUIDに設定し取得する
+        
+        :return: カスタマイズしたファイル名を含む画像パス
+        '''
+        prefix = "stamp_images/"
+        new_filename = str(uuid4()).replace("-", "")
+        extension = filename.split(".")[-1]
+        
+        return f"{prefix}{new_filename}.{extension}"
+    
     name = models.CharField(
         max_length=128,
         unique=True,
     )
+    '''スタンプ名'''
     
     latitude = models.FloatField(
         verbose_name="latitude",
     )
+    '''緯度'''
     
     longitude = models.FloatField(
         verbose_name="longitude",
     )
+    '''経度'''
+
+    stamp_image = models.ImageField(
+        verbose_name="stamp_image",
+        upload_to=get_uuid_image_path, # type: ignore
+        null=True, # データ生成時のnullはOK
+        blank=False, # 画像アップロード時のblankはNG
+    )
+    '''スタンプ画像'''
     
-    # スタンプを獲得したユーザ
     users = models.ManyToManyField(
         to=User,
         verbose_name="users_own_stamp",
     )
+    '''スタンプを獲得したユーザ'''
 
     class Meta:
         constraints = [
-            # 緯度と経度による重複を許可しない
             models.UniqueConstraint(
                 fields=[
                     "latitude",
@@ -34,6 +56,7 @@ class StampPin(models.Model):
                 name="stamp_pin_unique",
             ),
         ]
+        '''緯度と経度による重複を許可しない'''
     
     def __str__(self) -> str:
         return self.name
