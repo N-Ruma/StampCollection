9686fcc28df9a2976085844982d9e8a41e0f5767
Kotaro-PRG <23026157@muroran-it.ac.jp>
2025-11-30 13:53:51 +0900

    画像編集をグレースケールにして保存するための処理を追加しました
    先ほどのファイルだと実行ができなかったため、テスト用としてグレースケールにして保存するだけの処理を実装しました。(11/29)

--- views.py
+++ views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
 from .models import StampPin
-from .forms import StampPinForm 
+from .forms import StampPinForm
 #追加(11/28)
 from PIL import Image,ImageFilter,ImageOps
 import io
@@ -23,47 +23,34 @@ def add_stamp_pin_view(request):
     if request.method == "POST":
         form = StampPinForm(request.POST, request.FILES)
         if form.is_valid():
-            
+           
             obj = form.save(commit=False)
             #return redirect("home")
-        
-        upload_img = request.FILES["stamp_image"] #ここから画像編集に関する処理
-        img = Image.open(upload_img).convert("RGB")
-        
-        img = ImageOps.grayscale(img)#グレースケール追加
-        
-        img = img.filter(ImageFilter.BLUR)#ぼかし
-        
-        img = img.filter(ImageFilter.SHARPEN)#シャープ
-        
-        img = img.filter(ImageFilter.EDGE_ENHANCE)#エッジ強調
-        
-        sepia = []
-        r,g,b = img.split()
-        
-        for channel in (r,g,b):
-            sepia_channel = channel.point(lambda p:p*0.9)
-            
-            sepia.append(sepia_channel)
-            
-            img = Image.merge('RGB',sepia)
-            
-        buffer = io.BytesIO()
-        img.save(buffer,format =img.format or "JPEG")
-        
-        buffer.seek(0)
-        
-        obj.stamp_image.save(upload_img.name,ContentFile(buffer.read()),save = False)
-        
-        obj.save()
-        
+       
+            upload_img = request.FILES["stamp_image"] #ここから画像編集に関する処理
+            img = Image.open(upload_img)
+       
+            img = img.convert("L")#グレースケール追加(エラーが出てしまったため、動くかどうかの確認でグレースケールのみ実行)
+            img = img.convert("RGB")
+       
+               
+            buffer = io.BytesIO()
+               
+            img.save(buffer,format =img.format or "JPEG")
+               
+            buffer.seek(0)
+               
+            obj.stamp_image.save(upload_img.name,ContentFile(buffer.read()),save = False)
+               
+            obj.save()
+       
         return redirect("home") #ここまで追加(11/29)
-        
-        
-        
-        
-        
-        
+       
+       
+       
+       
+       
+       
     else:
         form = StampPinForm()
     context["form"] = form
