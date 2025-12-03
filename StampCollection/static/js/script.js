f660ffe4e88824ec9b3e8facc090f66c2fe40eee
N-Ruma <23026170@muroran-it.ac.jp>
2025-12-03 14:32:53 +0000

    読みやすくなるよう, 処理をscript.jsに矢印関数として移行しました． 代わりにtest_js.htmlからscript処理を削除しました．

--- StampCollection/static/js/script.js
+++ StampCollection/static/js/script.js
@@ -19,3 +19,40 @@ const calculate_image_size = (image_width, image_height, canvas_width, canvas_he
         return adjust_size_height(canvas_height, image_aspect_ratio, scale);
     }
 }
+
+const draw_image = (stamp_id, stamp_url) => {
+    const canvas = document.getElementById("canvas_" + stamp_id);
+    console.log("canvas_" + stamp_id);
+    const ctx = canvas.getContext("2d");
+
+    let image = new Image();
+    image.src = stamp_url;
+
+    image.onload = () => {
+        const canvas_width = canvas.width;
+        const canvas_height = canvas.height;
+        const image_width = image.width;
+        const image_height = image.height;
+        const { draw_width, draw_height } = calculate_image_size(image_width, image_height, canvas_width, canvas_height);
+
+        // blur filter
+        ctx.filter = "blur(5px)";
+
+        // draw image
+        const x = (canvas_width - draw_width) / 2;
+        const y = (canvas_height - draw_height) / 2;
+        ctx.drawImage(image, x, y, draw_width, draw_height);
+
+        // get pixel data
+        const src = ctx.getImageData(0, 0, canvas_width, canvas_height);
+        let pixel = src.data; // [r, g, b, a, r, g, b, a, ...]
+
+        // gray scale
+        for (let i = 0; i < pixel.length; i += 4) {
+            const color = 0.2126 * pixel[i] + 0.7152 * pixel[i + 1] + 0.0722 * pixel[i + 2];
+            pixel[i] = pixel[i + 1] = pixel[i + 2] = color;
+        }
+
+        ctx.putImageData(src, 0, 0);
+    }
+}
