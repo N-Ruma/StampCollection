0b4be6c9f093eda00d053c28468c43a8e33c2b65
N-Ruma <23026170@muroran-it.ac.jp>
2025-12-03 17:05:53 +0900

    Update views.py

--- StampCollection/stampapp/views.py
+++ StampCollection/stampapp/views.py
@@ -23,30 +23,8 @@ def add_stamp_pin_view(request):
     if request.method == "POST":
         form = StampPinForm(request.POST, request.FILES)
         if form.is_valid():
-            # -------------------------- 画像編集処理 --------------------------
-            data = form.save(commit=False) # フォームデータの取得
-            upload_img = request.FILES["stamp_image"]
-            img = Image.open(upload_img).convert("RGB")
-            img = img.filter(ImageFilter.BLUR) # ぼかし
-            img = img.filter(ImageFilter.SHARPEN) # シャープ
-            img = img.convert("L") # グレースケール
-            img = img.filter(ImageFilter.EDGE_ENHANCE) # エッジ強調
-            # セピア化
-            Image.merge(
-                mode="RGB",
-                bands=(
-                    img.point(lambda x: x * 240 / 255),
-                    img.point(lambda x: x * 200 / 255),
-                    img.point(lambda x: x * 145 / 255)
-                )
-            )
-
             # -------------------------- データ追加処理 --------------------------
-            buffer = io.BytesIO()
-            img.save(buffer, format=img.format or "JPEG")
-            buffer.seek(0)
-            data.stamp_image.save(upload_img.name, ContentFile(buffer.read()), save=False)
-            data.save()
+            form.save()
             return redirect("home")
     else:
         form = StampPinForm()
