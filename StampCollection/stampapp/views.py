dfc358f8710265944d30f32275d2df6c3149aee2
Kotaro-0714 <23026157@muroran-it.ac.jp>
2025-12-13 17:53:43 +0000

    ログインされていない状態でマップが表示されないよう修正

--- StampCollection/stampapp/views.py
+++ StampCollection/stampapp/views.py
@@ -65,6 +65,7 @@ def stamp_list_view(request):
 
     return render(request, template_name, context)
 
+@login_required
 def map_view(request):
     template_name = "stampapp/map.html"
     context = {}
