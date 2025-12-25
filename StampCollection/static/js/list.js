afef5443a60376e060bbdfd19c33fd02dc1c391d
N-Ruma <23026170@muroran-it.ac.jp>
2025-12-25 04:48:32 +0000

    ボタンの追加(機能未実装)

---
+++ StampCollection/static/js/list.js
@@ -0,0 +1,28 @@
+let current_sort = 0;
+// 0: 追加順
+// 1: 昇順
+// 2: 降順
+// 3: 獲得済み
+// 4: 未獲得
+
+const toggle_sort = () => {
+    switch (current_sort) {
+        case 0:
+            current_sort = 1;
+            break;
+        case 1:
+            current_sort = 2;
+            break;
+        case 2:
+            current_sort = 3;
+            break;
+        case 3:
+            current_sort = 4;
+            break;
+        case 4:
+            current_sort = 0;
+            break;
+        default:
+            break;
+    }
+};
