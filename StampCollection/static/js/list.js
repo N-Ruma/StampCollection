5cfb324f9cae91cf358b98e05cf4cddff6b3eb3d
N-Ruma <23026170@muroran-it.ac.jp>
2025-12-30 19:02:04 +0900

    軽微な修正

--- StampCollection/static/js/list.js
+++ StampCollection/static/js/list.js
@@ -7,22 +7,22 @@ let current_sort = 0;
 
 const toggle_sort = () => {
     switch (current_sort) {
-        case 0:
-            current_sort = 1;
-            break;
-        case 1:
-            current_sort = 2;
-            break;
-        case 2:
-            current_sort = 3;
-            break;
-        case 3:
-            current_sort = 4;
-            break;
-        case 4:
-            current_sort = 0;
-            break;
-        default:
-            break;
+    case 0:
+        current_sort = 1;
+        break;
+    case 1:
+        current_sort = 2;
+        break;
+    case 2:
+        current_sort = 3;
+        break;
+    case 3:
+        current_sort = 4;
+        break;
+    case 4:
+        current_sort = 0;
+        break;
+    default:
+        break;
     }
 };
